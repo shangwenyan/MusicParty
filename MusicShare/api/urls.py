4d20452e6623ba594a7184124e5eda969491b970
shangwenyan <y739249456@gmail.com>
2021-01-11 00:13:51 -0500

    code join room page

--- MusicShare/api/urls.py
+++ MusicShare/api/urls.py
@@ -1,7 +1,9 @@
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 from django.urls import path
 
 urlpatterns = [
     path('room',RoomView.as_view()),
-    path('create-room',CreateRoomView.as_view())
+    path('create-room',CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
 ]
